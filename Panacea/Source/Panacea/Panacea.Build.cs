af57e812aa4e5eb037ebd133faca75998121072b
Oleksandr Smirnov <116755622+oophiuchuss@users.noreply.github.com>
2024-08-07 15:45:18 +0200

    Update Panacea.Build.cs

--- Panacea/Source/Panacea/Panacea.Build.cs
+++ Panacea/Source/Panacea/Panacea.Build.cs
@@ -8,9 +8,17 @@ public class Panacea : ModuleRules
     {
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore",
-            "EnhancedInput", "UMG","Json", "JsonUtilities"
-            , "MetasoundEngine"
+        PublicDependencyModuleNames.AddRange(new string[] {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "InputCore",
+            "EnhancedInput",
+            "UMG",
+            "PhysicsCore",
+            "Chaos",
+            "GeometryCollectionEngine",
+            "MetasoundEngine"
         });
 
         PublicIncludePaths.AddRange(
