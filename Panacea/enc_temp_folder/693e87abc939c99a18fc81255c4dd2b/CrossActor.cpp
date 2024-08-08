7d1e39db5605071720f68e36bedd8f667bcaa523
Laurel Parsons <34589706+LaurelParsons@users.noreply.github.com>
2024-08-08 11:39:53 +0200

    Bugfixes

--- Panacea/enc_temp_folder/693e87abc939c99a18fc81255c4dd2b/CrossActor.cpp
+++ Panacea/enc_temp_folder/693e87abc939c99a18fc81255c4dd2b/CrossActor.cpp
@@ -21,7 +21,7 @@ ACrossActor::ACrossActor()
 	SwitchComponent = CreateDefaultSubobject<USwitchComponent>(TEXT("SwitchComponent"));
 	SwitchComponent->SetupAttachment(RootComponent);
 
-	MinimumDegrees = 100.0f;
+	MinimumDegrees = 120.0f;
 }
 
 void ACrossActor::BeginPlay()
